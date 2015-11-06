c329bcb9d44c5f1237440da1897162c8460f9042
bfrager <btfrager@gmail.com>
2015-11-05 17:05:31 -0800

    commented out :themes through :quotes in posts.rb because of heroku error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :quote
   has_and_belongs_to_many :boards
-  has_many :themes, through :quotes
+  # has_many :themes, through :quotes
 
   has_attached_file :image, styles: { medium: "300x300" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
